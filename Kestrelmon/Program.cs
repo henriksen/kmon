50f58eea837745f8894916f34a9288ba0c24f53e
Glenn F. Henriksen <glenn@henriksen.no>
2014-11-17 21:46:42 +0100

    Initial commit

---
+++ Kestrelmon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kestrelmon
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
