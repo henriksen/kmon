699d9a49b4fc4c58f214b0696115109856363720
spboyer <spboyer@live.com>
2014-11-19 21:25:38 -0500

    Added functionality to start support for OSX

--- Kestrelmon/Program.cs
+++ Kestrelmon/Program.cs
@@ -10,25 +10,7 @@ namespace Kestrelmon
 
             Process process = new Process();
             process.StartInfo.FileName = "nodemon";
-
-            //if (args != null)
-            //{
-            //    string monArgs = "";
-
-            //    foreach (string a in args)
-            //    {
-
-            //        monArgs += " " + a;
-            //    }
-
-            //    process.StartInfo.Arguments += monArgs;
-            //}
-
-            //process.StartInfo.Arguments += " --exec \"cmd /c k web\" ";
-
             process.StartInfo.Arguments = ParseArgs(args);
-
-            Console.WriteLine(process.StartInfo.Arguments);
             
             process.Start();
             process.WaitForExit();
@@ -40,9 +22,9 @@ namespace Kestrelmon
             var monArgs = "";
             var options = new Options(args);
 
-           if (!options.Errors)
+            if (!options.Errors)
             {
-                monArgs = string.Format("--ext {0} --exec \"cmd /c k {1} \"", options.FileExtensions, options.Server);
+                monArgs = options.ParsedArgs();
             }
 
             return monArgs;
