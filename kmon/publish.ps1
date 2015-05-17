2a3f0f0ce2e6b0937b463509ff9437bf104ac0eb
Glenn F. Henriksen <glenn@henriksen.no>
2015-05-18 00:53:55 +0200

    Updated nuget publish file to use dnu instead of kpm

--- kmon/publish.ps1
+++ kmon/publish.ps1
@@ -1,3 +1,2 @@
-kpm build --configuration release
 $latestNupkg = Get-ChildItem -Exclude *symbols* bin\release\*.nupkg | sort-object name -descending | select -First 1
 nuget push $latestNupkg.FullName
