4afb1602e4fbb0ac2c3be4ab94bdde714514964c
spboyer <spboyer@live.com>
2015-05-17 12:09:11 -0400

    changes to demo for namepsaces

---
+++ kmon.demo-dnx/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kmon.demo_dnx
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
